f64d8132e2d68b810cc6a35741ff5a76c5673a6f
Colin <vogon@outlook.com>
2013-11-10 13:54:28 -0800

    initial node hello

---
+++ app.js
@@ -0,0 +1,9 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+	res.send('hello world');
+});
