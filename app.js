1961e56c06718ec551cfb5618c8a78f603d10797
Colin <vogon@outlook.com>
2013-12-21 15:13:26 -0800

    express-minify

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    minify = require('express-minify');
 
 server.listen(80);
 
 app.use(express.logger());
 app.use(express.static(__dirname + "/public"));
-// app.use('/jquery', express.static(require.resolve('jquery-browser')));
+app.use(minify());
+app.use('/lib/bootstrap', express.static(require.resolve('bootstrap-browser')));
+app.use('/lib/jquery', express.static(require.resolve('jquery-browser')));
 
 io.sockets.on('connection', function (socket) {
   var intervalId = setInterval(function () {
